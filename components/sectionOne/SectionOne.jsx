b5407e23ff67fd89f1881e8c02c3d008fcb83b91
Jeff Oliver <jeffoliver3384@gmail.com>
2020-08-15 18:21:03 -0400

    created some  global styles for color and text in global.scss

---
+++ components/sectionOne/SectionOne.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SectionOne.module.scss";
+
+const SectionOne = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hi, I'm Jeffrey Oliver</h1>
+        <p>This is a sentence</p>
+        <a href='#'>Link</a>
+      </div>
+    </section>
+  );
+};
+
+export default SectionOne;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+
+import SectionOne from "../components/sectionOne/SectionOne";
 
 const Home = () => (
   <div className={styles.container}>
     <Head>
       <title>Jeff Oliver | Professional Portfolio</title>
       <link rel='icon' href='/favicon.ico' />
+
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1'
+      ></meta>
     </Head>
-    <main>
-      <h1>Home</h1>
-    </main>
+    <SectionOne />
   </div>
 );
 
