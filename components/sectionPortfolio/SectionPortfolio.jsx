132dd8e557249d83c28aadf9d11d63bd3f843fda
Jeff Oliver <jeffoliver3384@gmail.com>
2020-09-03 16:00:44 -0400

    added color mode to section portfolio

--- components/sectionPortfolio/SectionPortfolio.jsx
+++ components/sectionPortfolio/SectionPortfolio.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Carousel from "../carousel/Carousel";
-import styles from "./SectionPortfolio.module.scss";
+import { s3Lt, s3Bl, s3Gn, s3Pl } from "./SectionPortfolio.module.scss";
 
-const SectionPortfolio = () => {
+const SectionPortfolio = ({ colorMode }) => {
+  let s3Mode;
+
+  if (colorMode === "lightMode") {
+    s3Mode = s3Lt;
+  } else if (colorMode === "blueMode") {
+    s3Mode = s3Bl;
+  } else if (colorMode === "greenMode") {
+    s3Mode = s3Gn;
+  } else {
+    s3Mode = s3Pl;
+  }
+
   return (
-    <section className={styles.s3}>
+    <section className={s3Mode}>
       <div className='main-container'>
         <h3>Projects</h3>
-        <Carousel />
+        <Carousel colorMode={colorMode} />
       </div>
     </section>
   );
