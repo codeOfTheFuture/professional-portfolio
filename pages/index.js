fc1625704fa166c6999deefefbe251c974026c11
Jeff Oliver <jeffoliver3384@gmail.com>
2023-01-18 18:59:30 -0500

    fix dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
 import SectionMain from "../components/SectionMain";
 import SectionAbout from "../components/SectionAbout";
 import SectionPortfolio from "../components/SectionPortfolio";
@@ -9,6 +7,7 @@ import fetchSocials from "../utils/fetchSocials";
 import fetchProjects from "../utils/fetchProjects";
 import fetchSkills from "../utils/fetchSkills";
 import fetchResumeUrl from "../utils/fetchResumeUrl";
+import Head from "next/head";
 
 const Home = ({ pageInfo, socials, projects, skills, resumeUrl }) => {
   const { name, profilePic, whatIDo, aboutMe, topExpertise, image } = pageInfo;
@@ -16,19 +15,11 @@ const Home = ({ pageInfo, socials, projects, skills, resumeUrl }) => {
   return (
     <div>
       <Head>
-        <title>{name} | Professional Portfolio</title>
-        <link rel="icon" href="/Hopstarter-3d-Cartoon-Web-Coding.ico" />
+        <title>Jeff Oliver | Professional Portfolio</title>
 
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"></meta>
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
-          rel="stylesheet"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&family=Roboto:wght@500&family=Russo+One&display=swap"
-          rel="stylesheet"></link>
       </Head>
       <SectionMain name={name} profilePic={profilePic} whatIDo={whatIDo} />
       <SectionAbout
